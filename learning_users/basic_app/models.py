a5bd6e9f7c8f3c0d09327c0fd4a319bb08c494f0
Esther <esther@localhost.localdomain>
2019-05-07 13:15:59 +0300

    first commit

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+     #create a relationship
+     user=models.OneToOneField(User,on_delete=models.CASCADE)
+     #UserAttribute
+     portfolio_site=models.URLField(blank=True)
+     profile_pic=models.ImageField(upload_to='profile_pics',blank=True)
+     def __str__(self):
+         return self.user.username
